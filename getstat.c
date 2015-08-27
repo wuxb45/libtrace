a7acb9de44bb6cabd34b24fc733626d4a1085f5c
Wu Xingbo <wuxb45@gmail.com>
2015-08-26 22:02:12 -0400

    stat

--- getstat.c
+++ getstat.c
@@ -12,6 +12,22 @@ struct slot {
 };
 
 #define N1 ((1024*1024*64))
+#define NX ((8192))
+
+// big->little
+  static int
+__comp(const void * const p1, const void * const p2)
+{
+  const uint64_t v1 = *((const uint64_t *)p1);
+  const uint64_t v2 = *((const uint64_t *)p2);
+  if (v1 > v2) {
+    return -1;
+  } else if (v1 < v2) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
 
   void
 one_get(struct slot ** const slots, const uint64_t hkey)
@@ -53,20 +69,60 @@ main(int argc, char ** argv)
       get++;
     }
   }
-  uint64_t xcount[1024] = {};
+  const uint64_t pk = get/1000u;
+  uint64_t xcount[NX] = {};
+  uint64_t y = 0;
+  uint64_t keycount = 0;
   for (uint64_t i = 0; i < N1; i++) {
     struct slot * const slot = slots[i];
     for (uint64_t j = 0; j < slot->nr; j++) {
       const uint64_t c = slot->entries[j].count;
-      if (c < 1024) {
+      keycount++;
+      if (c < NX) {
         xcount[c]++;
       } else {
-        printf("%016" PRIx64 " %6" PRIu64 "\n", slot->entries[j].hkey, slot->entries[j].count);
+        y++;
       }
     }
   }
-  for (uint64_t i = 0; i < 1024; i++) {
-    if (xcount[i]) printf("X %04" PRIu64 " %" PRIu64 "\n", i, xcount[i]);
+  uint64_t * const ycount = (typeof(ycount))malloc(sizeof(*ycount) * y);
+  const uint64_t yy = y;
+  y=0;
+  for (uint64_t i = 0; i < N1; i++) {
+    struct slot * const slot = slots[i];
+    for (uint64_t j = 0; j < slot->nr; j++) {
+      const uint64_t c = slot->entries[j].count;
+      if (c >= NX) {
+        ycount[y] = c;
+        y++;
+      }
+    }
   }
+  assert(yy==y);
+  qsort(ycount, yy, sizeof(ycount[0]), __comp);
+
+  uint64_t acc0 = 0;
+  uint64_t nacc = 0;
+  uint64_t nkey = 0;
+  for (uint64_t i = 0; i < yy; i++) {
+    nacc += ycount[i];
+    nkey ++;
+    if ((nacc - acc0) > pk) {
+      printf("nkey %" PRIu64 " (%lf) nacc %" PRIu64 " (%lf)\n", nkey, ((double)nkey)/((double)keycount), nacc, ((double)nacc)/((double)get));
+      acc0 = nacc;
+    }
+  }
+  for (uint64_t i = 0; i < NX; i++) {
+    nacc += (NX-i-1) * xcount[NX-i-1];
+    nkey += xcount[NX-i-1];
+    if ((nacc - acc0) > pk) {
+      printf("nkey %" PRIu64 " (%lf) nacc %" PRIu64 " (%lf)\n", nkey, ((double)nkey)/((double)keycount), nacc, ((double)nacc)/((double)get));
+      acc0 = nacc;
+    }
+  }
+  
+  //for (uint64_t i = 0; i < NX; i++) {
+    //if (xcount[i]) printf("X %04" PRIu64 " %" PRIu64 "\n", i, xcount[i]);
+  //}
   return 0;
 }
