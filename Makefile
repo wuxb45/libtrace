edb69fecc967acb7d34a52414527aa4018078642
Wu Xingbo <wuxb45@gmail.com>
2015-08-29 09:49:04 -0400

    simple keymap

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
-all : conv allstat getstat printer types
+all : keymap conv allstat getstat printer types
 % : %.c libtrace.h
 	clang -std=gnu11 -O3 -o $@ $< -llz4
---
+++ keymap.c
@@ -0,0 +1,73 @@
+#include <unistd.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include "libtrace.h"
+
+struct slot {
+  uint32_t alloc;
+  uint32_t nr;
+  uint64_t hkey[];
+};
+
+static uint64_t next_id = 0;
+
+#define N1 ((1024*1024*64))
+#define NX ((8192))
+
+// big->little
+  static int
+__comp(const void * const p1, const void * const p2)
+{
+  const uint64_t v1 = *((const uint64_t *)p1);
+  const uint64_t v2 = *((const uint64_t *)p2);
+  if (v1 > v2) {
+    return -1;
+  } else if (v1 < v2) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+  void
+one_key(struct slot ** const slots, const uint64_t hkey)
+{
+  struct slot * slot = slots[hkey % N1];
+  for (uint64_t i = 0; i < slot->nr; i++) {
+    if (slot->hkey[i] == hkey) {
+      return;
+    }
+  }
+  // not found
+  if (slot->nr == slot->alloc) {
+    slot = (typeof(slot))realloc(slot, sizeof(*slot) + ((slot->alloc + 4) * sizeof(slot->hkey[0])));
+    slots[hkey % N1] = slot;
+    slot->alloc += 4;
+  }
+  slot->hkey[slot->nr] = hkey;
+  slot->nr++;
+  next_id++;
+}
+
+int
+main(int argc, char ** argv)
+{
+  struct slot ** const slots = (typeof(slots))malloc(sizeof(*slots) * N1);
+  for (uint64_t i = 0; i < N1; i++) {
+    struct slot * const slot = (typeof(slot))malloc(sizeof(*slot) + (8 * sizeof(slot->hkey[0])));
+    slot->alloc = 8;
+    slot->nr = 0;
+    slots[i] = slot;
+  }
+  struct event e;
+  uint64_t ts = 0;
+  while(next_event(stdin, &ts, &e)) {
+    one_key(slots, e.hkey);
+  }
+  for (uint64_t i = 0; i < N1; i++) {
+    struct slot * const slot = slots[i];
+    fwrite(slot->hkey, sizeof(slot->hkey[0]), slot->nr, stdout);
+  }
+  fflush(stdout);
+  return 0;
+}
