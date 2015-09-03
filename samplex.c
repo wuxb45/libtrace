19d1c89d91dcfd9f7101678276060125d42622a9
Wu Xingbo <wuxb45@gmail.com>
2015-09-02 20:31:13 -0400

    update samplex

--- samplex.c
+++ samplex.c
@@ -16,6 +16,7 @@ main(int argc, char ** argv)
   uint32_t keys[32];
   struct samplex xarr[256];
   uint64_t xid = 0;
+  uint32_t max_key = 0;
   for (;;) {
     if (fread(&ops, sizeof(ops), 1, stdin) != 1) break;
     const size_t nkeys = fread(keys, sizeof(keys[0]), 32, stdin);
@@ -24,6 +25,7 @@ main(int argc, char ** argv)
         const uint32_t op = (ops >> (i<<1)) & UINT64_C(0x3);
         xarr[xid].op = op;
         xarr[xid].keyx = keys[i] >> 6;
+        if (xarr[xid].keyx > max_key) max_key = xarr[xid].keyx;
         xid++;
         if (xid == 256) {
           fwrite(xarr, sizeof(xarr[0]), 256, stdout);
@@ -35,6 +37,8 @@ main(int argc, char ** argv)
   if (xid) {
     fwrite(xarr, sizeof(xarr[0]), xid, stdout);
   }
+  fprintf(stderr, "max generated key %" PRIu32 "\n", max_key);
+  fflush(stderr);
   fflush(stdout);
   return 0;
 }
