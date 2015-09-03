363b4703ffeff6a243c8f35d5503c0070e70f70d
Wu Xingbo <wuxb45@gmail.com>
2015-09-02 20:11:37 -0400

    fix argv

--- lru.c
+++ lru.c
@@ -9,8 +9,8 @@ main(int argc, char ** argv)
     printf("usage: %s <trace> <vlen> <nr_keys> <max_cap>\n", argv[0]);
     exit(0);
   }
-  const uint32_t nr_keys = strtoull(argv[5], NULL, 10);
-  const uint64_t max_cap = strtoull(argv[6], NULL, 10);
+  const uint32_t nr_keys = strtoull(argv[3], NULL, 10);
+  const uint64_t max_cap = strtoull(argv[4], NULL, 10);
   runtrace(argv[1], argv[2], nr_keys, max_cap, &lru_api);
   exit(0);
 }
