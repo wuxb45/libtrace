e287ab31459e1e6d60dd1a3dfa4826afc2a7e3ed
Wu Xingbo <wuxb45@gmail.com>
2015-09-06 01:39:35 -0400

    more tags

--- runlru3.sh
+++ runlru3.sh
@@ -3,17 +3,21 @@ mb=$((1024*1024))
 
 rungroup()
 {
-  trace=${1}
+  local tool=${1}
+  local trace=${2}
   mkdir -p trace
-    x=${2}
+  x=${3}
   for i in $(seq 1 24); do
     sz=$(($mb * $x))
-    ./lru ${trace}/sp ${trace}/vlen $(cat ${trace}/keys) ${sz} > trace/${trace}-${x}.txt &
+    ./${tool} ${trace}/sp ${trace}/vlen $(cat ${trace}/keys) ${sz} > trace/${tool}-${trace}-${x}.txt &
     x=$(($x + ($x / 4)))
   done
   wait
 }
 
-rungroup etc 20
-rungroup usr 5
-rungroup app 10
+rungroup lru etc 20
+rungroup lru usr 5
+rungroup lru app 10
+rungroup lirs etc 20
+rungroup lirs usr 5
+rungroup lirs app 10
