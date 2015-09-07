5d6d3a20bd1eceb9d81bda0da14ae00b64c1d6ad
Wu Xingbo <wuxb45@gmail.com>
2015-09-07 14:03:38 -0400

    versatile samplex

--- samplex.c
+++ samplex.c
@@ -12,6 +12,13 @@ struct samplex {
   int
 main(int argc, char ** argv)
 {
+  if (argc != 2) {
+    printf("usage: %s <shift>\n", argv[0]);
+    exit(0);
+  }
+  const uint64_t shift = strtoull(argv[1], NULL, 10);
+  assert(shift < 32);
+  const uint32_t mask = (1u << shift) - 1u;
   uint64_t ops = 0;
   uint32_t keys[32];
   struct samplex xarr[256];
@@ -21,10 +28,10 @@ main(int argc, char ** argv)
     if (fread(&ops, sizeof(ops), 1, stdin) != 1) break;
     const size_t nkeys = fread(keys, sizeof(keys[0]), 32, stdin);
     for (uint64_t i = 0; i < nkeys; i++) {
-      if ((keys[i] & 0x3f) == 0) {
+      if ((keys[i] & mask) == 0) {
         const uint32_t op = (ops >> (i<<1)) & UINT64_C(0x3);
         xarr[xid].op = op;
-        xarr[xid].keyx = keys[i] >> 6;
+        xarr[xid].keyx = keys[i] >> shift;
         if (xarr[xid].keyx > max_key) max_key = xarr[xid].keyx;
         xid++;
         if (xid == 256) {
