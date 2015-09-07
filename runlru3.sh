4590d8ba58ca1e66d8eee1eca0ee9e4b0cbb5306
Wu Xingbo <wuxb45@gmail.com>
2015-09-06 23:25:32 -0400

    update runlru3.sh

--- runlru3.sh
+++ runlru3.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 mb=$((1024*1024))
 
+mkdir -p newstat
 rungroup()
 {
   local tool=${1}
   local trace=${2}
-  mkdir -p trace
-  x=${3}
-  for i in $(seq 1 24); do
-    sz=$(($mb * $x))
-    echo ./${tool} ${trace}/sp ${trace}/vlen $(cat ${trace}/keys) ${sz} > trace/${tool}-${trace}-${x}.txt &
+  local x=${3}
+  local rounds=${4}
+  local paramx=${5}
+  
+  for i in $(seq 1 ${rounds}); do
+    local sz=$(($mb * $x))
+    ./${tool} ${trace}/sp ${trace}/vlen $(cat ${trace}/keys) ${sz} ${paramx} > newstat/${trace}-${x}-${tool}.txt &
     x=$(($x + ($x / 4)))
   done
-  wait
 }
 
-rungroup lru etc 20
-rungroup lru usr 5
-rungroup lru app 10
-rungroup lirs etc 20
-rungroup lirs usr 5
-rungroup lirs app 10
+rungroup lru etc 20 24
+rungroup lrux etc 20 24 262171838
+wait
+rungroup lirs etc 20 24
+
+rungroup lru app 10 24
+wait
+rungroup lrux app 10 24 258158358
+rungroup lirs app 10 24
+wait
+
+rungroup lru usr 8 16
+rungroup lrux usr 8 16 17199206
+rungroup lirs usr 8 16
+wait
