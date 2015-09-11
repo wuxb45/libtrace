28144eb6450c43ebececd530c03f46714f4ba200
Wu Xingbo <wuxb45@gmail.com>
2015-09-11 18:54:28 -0400

    runlru3.sh: groups

--- runlru3.sh
+++ runlru3.sh
@@ -15,83 +15,127 @@
 #bg_run_group lirs usr  8 16 dir
 
 #### 80%
-#bg_run_group lrux etc 20 24 dir 608372793
-#bg_run_group lrux app 10 24 dir 504676200
-#bg_run_group lrux usr  8 16 dir 27162703
-
-#sys
-#bg_run_group lru  sys  5 20 dir
-#bg_run_group lrux sys  5 20 dir 185366086
-#bg_run_group lirs sys  5 20 dir
-
 #80% group
-bg_run_group lru  etc 20 24 noset
-bg_run_group lrux etc 20 24 noset 608372793
-bg_run_group lirs etc 20 24 noset
-wait
+group1 ()
+{
+  bg_run_group lru  etc 30 22 noset
+  bg_run_group lrux etc 30 22 noset 608372793
+  wait
+}
 
-bg_run_group lru  app 10 24 noset
-bg_run_group lrux app 10 24 noset 504676200
-bg_run_group lirs app 10 24 noset
-wait
+group2 ()
+{
+  bg_run_group lirs etc 30 22 noset
+  bg_run_group arc  etc 30 22 noset
+  wait
+}
 
-bg_run_group lru  usr  8 20 noset
-bg_run_group lrux usr  8 20 noset 27162703
-bg_run_group lirs usr  8 20 noset
-wait
+group3 ()
+{
+  bg_run_group lru  app 10 24 noset
+  bg_run_group lrux app 10 24 noset 504676200
+  wait
+}
 
-#80% fixed points x1 x1.5 x2 x2.5 x3
-#etc
-bg_run_one lru etc 580 points
-bg_run_one lru etc 870 points
-bg_run_one lru etc 1160 points
-bg_run_one lru etc 1450 points
-bg_run_one lru etc 1740 points
+group4 ()
+{
+  bg_run_group lirs app 10 24 noset
+  bg_run_group arc  app 10 24 noset
+  wait
+}
 
-bg_run_one lirs etc 580 points
-bg_run_one lirs etc 870 points
-bg_run_one lirs etc 1160 points
-bg_run_one lirs etc 1450 points
-bg_run_one lirs etc 1740 points
+group5 ()
+{
+  bg_run_group lru  usr  8 20 noset
+  bg_run_group lrux usr  8 20 noset 27162703
+  wait
+}
 
-bg_run_one lrux etc 870 points 608372793
-bg_run_one lrux etc 1160 points 608372793
-bg_run_one lrux etc 1450 points 608372793
-bg_run_one lrux etc 1740 points 608372793
+group6 ()
+{
+  bg_run_group lirs usr  8 20 noset
+  bg_run_group arc  usr  8 20 noset
+  wait
+}
 
-#app
-bg_run_one lru app 481 points
-bg_run_one lru app 722 points
-bg_run_one lru app 963 points
-bg_run_one lru app 1203 points
-bg_run_one lru app 1444 points
+group7 ()
+{
+  #80% fixed points x1 x1.5 x2 x2.5 x3
+  #etc
+  bg_run_one lru etc 580 points
+  bg_run_one lru etc 870 points
+  bg_run_one lru etc 1160 points
+  bg_run_one lru etc 1450 points
+  bg_run_one lru etc 1740 points
 
-bg_run_one lirs app 481 points
-bg_run_one lirs app 722 points
-bg_run_one lirs app 963 points
-bg_run_one lirs app 1203 points
-bg_run_one lirs app 1444 points
+  bg_run_one lirs etc 580 points
+  bg_run_one lirs etc 870 points
+  bg_run_one lirs etc 1160 points
+  bg_run_one lirs etc 1450 points
+  bg_run_one lirs etc 1740 points
 
-bg_run_one lrux app 722 points 504676200
-bg_run_one lrux app 963 points 504676200
-bg_run_one lrux app 1203 points 504676200
-bg_run_one lrux app 1444 points 504676200
+  bg_run_one arc etc 580 points
+  bg_run_one arc etc 870 points
+  bg_run_one arc etc 1160 points
+  bg_run_one arc etc 1450 points
+  bg_run_one arc etc 1740 points
 
-#usr
-bg_run_one lru usr 26 points
-bg_run_one lru usr 39 points
-bg_run_one lru usr 52 points
-bg_run_one lru usr 65 points
-bg_run_one lru usr 78 points
+  bg_run_one lrux etc 870 points 608372793
+  bg_run_one lrux etc 1160 points 608372793
+  bg_run_one lrux etc 1450 points 608372793
+  bg_run_one lrux etc 1740 points 608372793
 
-bg_run_one lirs usr 26 points
-bg_run_one lirs usr 39 points
-bg_run_one lirs usr 52 points
-bg_run_one lirs usr 65 points
-bg_run_one lirs usr 78 points
+  #app
+  bg_run_one lru app 481 points
+  bg_run_one lru app 722 points
+  bg_run_one lru app 963 points
+  bg_run_one lru app 1203 points
+  bg_run_one lru app 1444 points
 
-bg_run_one lrux usr 39 points 27162703
-bg_run_one lrux usr 52 points 27162703
-bg_run_one lrux usr 65 points 27162703
-bg_run_one lrux usr 78 points 27162703
-wait
+  bg_run_one lirs app 481 points
+  bg_run_one lirs app 722 points
+  bg_run_one lirs app 963 points
+  bg_run_one lirs app 1203 points
+  bg_run_one lirs app 1444 points
+}
+
+group8 ()
+{
+
+  bg_run_one arc app 481 points
+  bg_run_one arc app 722 points
+  bg_run_one arc app 963 points
+  bg_run_one arc app 1203 points
+  bg_run_one arc app 1444 points
+
+  bg_run_one lrux app 722 points 504676200
+  bg_run_one lrux app 963 points 504676200
+  bg_run_one lrux app 1203 points 504676200
+  bg_run_one lrux app 1444 points 504676200
+
+  #usr
+  bg_run_one lru usr 26 points
+  bg_run_one lru usr 39 points
+  bg_run_one lru usr 52 points
+  bg_run_one lru usr 65 points
+  bg_run_one lru usr 78 points
+
+  bg_run_one lirs usr 26 points
+  bg_run_one lirs usr 39 points
+  bg_run_one lirs usr 52 points
+  bg_run_one lirs usr 65 points
+  bg_run_one lirs usr 78 points
+
+  bg_run_one arc usr 26 points
+  bg_run_one arc usr 39 points
+  bg_run_one arc usr 52 points
+  bg_run_one arc usr 65 points
+  bg_run_one arc usr 78 points
+
+  bg_run_one lrux usr 39 points 27162703
+  bg_run_one lrux usr 52 points 27162703
+  bg_run_one lrux usr 65 points 27162703
+  bg_run_one lrux usr 78 points 27162703
+  wait
+}
+${1}
