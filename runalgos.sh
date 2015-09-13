5e180091bd992a2ee4f306327ae226726b25bb38
Wu Xingbo <wuxb45@gmail.com>
2015-09-12 21:09:40 -0400

    output: group

--- runalgos.sh
+++ runalgos.sh
@@ -7,41 +7,41 @@
 #80% group
 glru ()
 {
-  bg_run_group lru  etc 30 22 noset
-  bg_run_group lru  usr  8 20 noset
+  bg_run_group lru  etc 30 22 group
+  bg_run_group lru  usr  8 20 group
   wait
-  bg_run_group lru  app 10 24 noset
-  bg_run_group lru  zip 60 24 noset
+  bg_run_group lru  app 10 24 group
+  bg_run_group lru  zip 60 24 group
   wait
 }
 
 glrux ()
 {
-  bg_run_group lrux etc 30 22 noset 608372793
-  bg_run_group lrux usr  8 20 noset 27162703
+  bg_run_group lrux etc 30 22 group 608372793
+  bg_run_group lrux usr  8 20 group 27162703
   wait
-  bg_run_group lrux app 10 24 noset 504676200
-  bg_run_group lrux zip 60 24 noset 275014743
+  bg_run_group lrux app 10 24 group 504676200
+  bg_run_group lrux zip 60 24 group 275014743
   wait
 }
 
 glirs ()
 {
-  bg_run_group lirs etc 30 22 noset
-  bg_run_group lirs usr  8 20 noset
+  bg_run_group lirs etc 30 22 group
+  bg_run_group lirs usr  8 20 group
   wait
-  bg_run_group lirs app 10 24 noset
-  bg_run_group lirs zip 60 24 noset
+  bg_run_group lirs app 10 24 group
+  bg_run_group lirs zip 60 24 group
   wait
 }
 
 garc ()
 {
-  bg_run_group arc  etc 30 22 noset
-  bg_run_group arc  usr  8 20 noset
+  bg_run_group arc  etc 30 22 group
+  bg_run_group arc  usr  8 20 group
   wait
-  bg_run_group arc  app 10 24 noset
-  bg_run_group arc  zip 60 24 noset
+  bg_run_group arc  app 10 24 group
+  bg_run_group arc  zip 60 24 group
   wait
 }
 
